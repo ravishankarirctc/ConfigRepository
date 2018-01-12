503d8e7c15264a96b66a1f241a2c6039c1f446cd
ravishankar9 <ravishankar9@USHYDRAVISHANK9.us.deloitte.com>
2018-01-12 12:47:35 +0530

    msg

---
+++ MavenExamplesParent/src/main/java/com/mycompany/parent/MavenExamplesParent/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.parent.MavenExamplesParent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
